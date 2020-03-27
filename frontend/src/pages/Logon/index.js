6d1f99d90219724e22a16ce2b4752373aba3c4d1
Diogo Simao da Silva <diogo.silva@orsegups.com.br>
2020-03-27 00:52:36 -0300

    final commit web application

--- frontend/src/pages/Logon/index.js
+++ frontend/src/pages/Logon/index.js
@@ -1,34 +1,55 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import { FiLogIn }  from 'react-icons/fi';
-import './styles.css'
+import React, { useState } from "react";
+import { Link, useHistory } from "react-router-dom";
+import { FiLogIn } from "react-icons/fi";
 
-import heroesImg from '../../assets/heroes.png'
-import logoImg from '../../assets/logo.svg'
+import "./styles.css";
+import logoimg from "../../assets/logo.svg";
+import Herosimg from "../../assets/heroes.png";
 
+import api from "../../services/api";
 
-export default function Logon(){
-    return (
-        <div className="logon-container">
-            <section className="form">
-                <img src={logoImg} alt="Be the hero!"/>
-                
-                <form>
-                    <h1>Faça seu logon</h1>
-                    
-                    <input placeholder="Sua ID"/>
-                    <Link to='/profile'>
-                        <button className="button" type="submit">Entrar</button>
-                    </Link>
+export default function Logon() {
+  const [id, setId] = useState("");
+  const history = useHistory();
 
-                    <Link className="back-link" to="/register">
-                        <FiLogIn size={16} color="#E02041" />
-                        Não tenho cadastro
-                    </Link>
-                </form>
-            </section>
+  async function handleLogin(e) {
+    e.preventDefault();
 
-            <img src={heroesImg} alt="Heroes"/>
-        </div>
-    )
+    try {
+      const res = await api.post("sessions", { id });
+
+      localStorage.setItem("ongId", id);
+      localStorage.setItem("ongName", res.data.name);
+
+      history.push("/profile");
+    } catch (error) {
+      alert("Falha ao fazer login, tente novamente");
+    }
+  }
+
+  return (
+    <div className="logon-container">
+      <section className="form">
+        <img src={logoimg} alt="Be the hero" />
+        <form onSubmit={handleLogin}>
+          <h1>Faça seu Logon</h1>
+
+          <input
+            placeholder="Sua id"
+            value={id}
+            onChange={e => setId(e.target.value)}
+          />
+          <button type="submit" className="button">
+            Entrar
+          </button>
+
+          <Link className="back-link" to="/register">
+            <FiLogIn size={16} color="#E02041" />
+            Não tenho cadastro
+          </Link>
+        </form>
+      </section>
+      <img src={Herosimg} alt="Heroes" />
+    </div>
+  );
 }
